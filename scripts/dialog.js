a9002ff038df167ee67fd6e68ac6da7d63f1aaf6
edipojoseoliveira@gmail.com <edipojoseoliveira@gmail.com>
2019-11-17 19:14:46 -0300

    Alterações para funcionalidade de redefinir a senha

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -17,16 +17,90 @@ var dialogsHTML =
 					'<div class="form-group">' +
 						'<input type="password" id="inputLoginSenha" class="form-control" placeholder="Senha">' +
 					'</div>' +
-					'<span>Se ainda não se cadastrou <a onclick="fechaLoginAbreCadastro()" href="#">clique aqui</a>!</span>' +
+					'<div class="form-group">' +
+						'<button type="button" class="btn btn-primary btn-block" onclick="contaEntrar()">Entrar</button>' +
+					'</div>' +
+					'<div class="form-group text-center">' +
+						'<span><a onclick="fechaLoginAbreRedefinirSenha()" href="#">Esqueci minha senha</a></span>' +
+					'</div>' +
+					'<div class="form-group text-center">' +
+						'<span>Ou se ainda não se cadastrou <a onclick="fechaLoginAbreCadastro()" href="#">clique aqui</a>!</span>' +
+					'</div>' +
 				'</form>' +
-			'</div>' + 
-			'<div class="modal-footer">' + 
-				'<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>' + 
-				'<button type="button" class="btn btn-primary" onclick="contaEntrar()">Entrar</button>' +
 			'</div>' +
 		'</div>' +
 	'</div>' +
 '</div>' +
+
+'<div class="modal fade" id="modalRedefinirSenha" tabindex="-1" role="dialog" aria-labelledby="redefinirSenhaModalCenterTitle" aria-hidden="true">' + 
+	'<div class="modal-dialog modal-dialog-centered" role="document">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header">' +
+				'<h5 class="modal-title" id="redefinirSenhaModalCenterTitle">Redefinir senha</h5>' +
+				'<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+					'<span aria-hidden="true">&times;</span>' +
+				'</button>' +
+			'</div>' +
+			'<div class="modal-body">' +
+				'<div id="mensagensRedefinirSenhha"></div>' +
+				'<form>' +
+					'<div class="form-group text-center">' +
+						'<span>Informe o mesmo e-mail que utilizou ao criar sua conta.</span>' +
+					'</div>' +
+					'<div class="form-group">' +
+						'<input type="mail" id="inputRedefinirSenhaEmail" class="form-control" placeholder="E-mail">' +
+					'</div>' +
+					'<div class="form-group">' +
+						'<button type="button" class="btn btn-primary btn-block" onclick="contaRedefinirSenha()">Enviar e-mail</button>' +
+					'</div>' +
+				'</form>' +
+			'</div>' +
+		'</div>' +
+	'</div>' +
+'</div>' +
+
+'<div class="modal fade" id="modalMensagemRedefinirSenha" tabindex="-1" role="dialog" aria-labelledby="mensagemRedefinirSenhaModalCenterTitle" aria-hidden="true">' + 
+	'<div class="modal-dialog modal-dialog-centered" role="document">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header">' +
+				'<h5 class="modal-title" id="mensagemRedefinirSenhaModalCenterTitle">Redefinir senha</h5>' +
+				'<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+					'<span aria-hidden="true">&times;</span>' +
+				'</button>' +
+			'</div>' +
+			'<div class="modal-body">' +
+				'<div class="alert alert-success" role="alert">' +
+					'<h4 class="alert-heading">E-mail enviado</h4>' +
+					'<p>Você deverá receber em breve um e-mail permitindo a redefinição de sua senha. Caso não encontre o e-mail verifique seus spams e lixo por favor.</p>' +
+				'</div>' +
+			'</div>' +
+			'<div class="modal-footer">' + 
+				'<button type="button" class="btn btn-primary" data-dismiss="modal">Entendi</button>' + 
+			'</div>' +
+		'</div>' +
+	'</div>' +
+'</div>' +
+
+'<div class="modal fade" id="modalMensagemSenhaRedefinida" tabindex="-1" role="dialog" aria-labelledby="mensagemSenhaRedefinidaModalCenterTitle" aria-hidden="true">' + 
+	'<div class="modal-dialog modal-dialog-centered" role="document">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header">' +
+				'<h5 class="modal-title" id="mensagemSenhaRedefinidaModalCenterTitle">Redefição de senha</h5>' +
+			'</div>' +
+			'<div class="modal-body">' +
+				'<div class="alert alert-success" role="alert">' +
+					'Sua senha foi atualizada com sucesso.' +
+				'</div>' +
+			'</div>' +
+			'<div class="modal-footer">' +
+				'<a href="https://www.edicursos.com.br/">' + 
+					'<button type="button" class="btn btn-primary">OK</button>' + 
+				'</a>'
+			'</div>' +
+		'</div>' +
+	'</div>' +
+'</div>' +
+
 '<div class="modal fade" id="modalCadastreSe" tabindex="-1" role="dialog" aria-labelledby="CadastreSeModalCenterTitle" aria-hidden="true">' + 
 	'<div class="modal-dialog modal-dialog-centered" role="document">' +
 		'<div class="modal-content">' +
@@ -63,6 +137,20 @@ var dialogsHTML =
 function fechaLoginAbreCadastro() {
 	$('#modalEntrar').modal('hide');
 	$('#modalCadastreSe').modal('show');
+}
+
+function fechaLoginAbreRedefinirSenha() {
+	$('#modalEntrar').modal('hide');
+	$('#modalRedefinirSenha').modal('show');
+}
+
+function fechaRedefinirSenhaAbreMensagem() {
+	$('#modalRedefinirSenha').modal('hide');
+	$('#modalMensagemRedefinirSenha').modal('show');
+}
+
+function abreMensagemSenhaRedefinida() {
+	$('#modalMensagemSenhaRedefinida').modal('show');
 }
 
 var dialogs = document.getElementById("dialogs");
--- scripts/service/conta-service.js
+++ scripts/service/conta-service.js
@@ -13,6 +13,36 @@ function limparMensagens() {
 	mensagensEntrar.innerHTML = '';
 }
 
+function exibirMensagemRedefinirSenha(mensagem) {
+	var mensagemHTML = 
+	'<div class="alert alert-danger" role="alert">' +
+		mensagem +
+	'</div>';
+
+	var mensagensRedefinirSenha = document.getElementById("mensagensRedefinirSenhha");
+	mensagensRedefinirSenha.innerHTML = mensagemHTML;
+}
+
+function limparMensagensRedefinirSenha() {
+	var mensagensRedefinirSenha = document.getElementById("mensagensRedefinirSenhha");
+	mensagensRedefinirSenha.innerHTML = '';
+}
+
+function exibirMensagemTelaRedefinirSenha(mensagem) {
+	var mensagemHTML = 
+	'<div class="alert alert-danger" role="alert">' +
+		mensagem +
+	'</div>';
+
+	var mensagensRedefinirSenha = document.getElementById("mensagensTelaRedefinirSenhha");
+	mensagensRedefinirSenha.innerHTML = mensagemHTML;
+}
+
+function limparMensagensTelaRedefinirSenha() {
+	var mensagensRedefinirSenha = document.getElementById("mensagensTelaRedefinirSenhha");
+	mensagensRedefinirSenha.innerHTML = '';
+}
+
 function validarCampos() {
 	if (document.getElementById('inputLoginEmail').value == null ||
 		document.getElementById('inputLoginEmail').value == '') {
@@ -57,5 +87,105 @@ function contaEntrar() {
 				//console.log('Requisição finalizada: ' + textStatus);
 			}
 		});
+	}
+}
+
+function contaRedefinirSenha() {
+	limparMensagensRedefinirSenha();
+	if (document.getElementById('inputRedefinirSenhaEmail').value == null ||
+		document.getElementById('inputRedefinirSenhaEmail').value == '') {
+		exibirMensagemRedefinirSenha('Digite seu e-mail por favor.');
+		return false;
+	} else {
+		var conta = new Object();
+		conta.email = document.getElementById('inputRedefinirSenhaEmail').value;
+		
+		$.ajax({
+			url: 'http://192.168.100.34:8080/EdiCursos/api/conta/enviar-email-redefinir-senha?email=' + conta.email,
+			headers: {
+		        'Content-Type': 'application/json',
+		        'Foo-Header': 'foo'
+		    },
+			type: 'GET',
+			dataType: 'json',
+			success: function(result, status, request) {
+				if (result.mensagem == 'E-mail enviado com sucesso!') {
+					//Tentando chamar o método que está no arquivo dialog.js
+					fechaRedefinirSenhaAbreMensagem();
+				} else {
+					exibirMensagemRedefinirSenha(result.mensagem);
+				}
+			},
+			error: function(request, status, erro) {
+				exibirMensagemRedefinirSenha('Erro ao tentar redefinir sua senha!');
+			},
+			complete: function (jqXHR, textStatus) {
+				//console.log('Requisição finalizada: ' + textStatus);
+			}
+		});
+	}		
+}
+
+function validarCamposParaRedefinirSenha() {
+	if (document.getElementById('inputSenha').value == null || document.getElementById('inputSenha').value == '') {
+		exibirMensagemTelaRedefinirSenha('Informe a nova senha!');
+		return false;
+	} else if (document.getElementById('inputSenhaConfirmacao').value == null || document.getElementById('inputSenhaConfirmacao').value == '') {
+		exibirMensagemTelaRedefinirSenha('Confirme a nova senha!');
+		return false;
+	} else if (document.getElementById('inputSenha').value != document.getElementById('inputSenhaConfirmacao').value) {
+		exibirMensagemTelaRedefinirSenha('As senhas informadas são diferentes, verifique as informações!');
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function salvarSenhaRedefinida() {
+	limparMensagensTelaRedefinirSenha();
+	
+	var query = location.search.slice(1);
+	var partes = query.split('&');
+	var data = {};
+	partes.forEach(function(parte) {
+		var chaveValor = parte.split('=');
+		var chave = chaveValor[0];
+		var valor = chaveValor[1];
+		data[chave] = valor;
+	});
+
+	if (validarCamposParaRedefinirSenha()) {
+		if (data['e'] != null) {
+			var conta = new Object();
+			//O e-mail da conta deve vir no parâmetro "e" da url
+			conta.email = data['e'];
+			conta.senha = document.getElementById('inputSenha').value;
+			
+			$.ajax({
+				url: 'http://192.168.100.34:8080/EdiCursos/api/conta/redefinir-senha',
+				headers: {
+					'Content-Type': 'application/json',
+					'Foo-Header': 'foo'
+				},
+				type: 'POST',
+				data: JSON.stringify(conta),
+				success: function(result, status, request) {
+					if (result.mensagem == 'Senha redefinida com sucesso!') {
+						//Tentando chamar o método que está no arquivo dialog.js
+						abreMensagemSenhaRedefinida();
+					} else {
+						exibirMensagemTelaRedefinirSenha(result.mensagem);
+					}
+				},
+				error: function(request, status, erro) {
+					exibirMensagemTelaRedefinirSenha('Erro ao tentar redefinir a senha: ' + JSON.stringify(erro));
+				},
+				complete: function (jqXHR, textStatus) {
+					console.log('Fim: ' + textStatus);
+				}
+			});
+		} else {
+			exibirMensagemTelaRedefinirSenha('Erro ao tentar redefinir sua senha, acesse seu e-mail e clique novamente no botão "Redefinir senha"!');
+		}
 	}
 }
