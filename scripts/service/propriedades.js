5e52159b761b3739c23a0697e08bd1321603dff0
Edipo Oliveira <edipojoseoliveira@gmail.com>
2021-02-23 22:30:17 -0300

    Alterações no layout das telas de aula, curso e index

--- scripts/service/propriedades.js
+++ scripts/service/propriedades.js
@@ -2,4 +2,4 @@
 //var urlSite = 'http://127.0.0.1:8887/';
 
 var urlServer = 'http://52.6.160.222:8080/';
-var urlSite = 'http://s3.edicursos.com.s3-website-us-east-1.amazonaws.com/';
+var urlSite = 'http://www.edicursos.com.br/';
